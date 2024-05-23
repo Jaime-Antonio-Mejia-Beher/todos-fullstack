3d3e73e7ec752a75cfeb12f8aaf408ddfdf71315
Jaime Mejia-Beher <jmejiabeher@gmail.com>
2024-05-23 11:41:09 -0600

    added index routes

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoConfig from "./config.js";
 import 'dotenv/config'
+import Todo from "./models/ToDoModel.js";
 
 const app = express();
 
@@ -8,6 +9,16 @@ const PORT = 8080
 
 app.get('/', (req, res) => {
     res.json("Hello from server...STILL WORKING")
+})
+
+app.get('/api/todos', async(req, res) => {
+    try {
+        const todos = await Todo.find() //request todos form database
+        res.status(200).json(todos) // send to client what we returned from database
+    } catch(error) {
+        console.log(err.message)
+        res.status(400).json(err)
+    }
 })
 
 app.listen(PORT, () => {
