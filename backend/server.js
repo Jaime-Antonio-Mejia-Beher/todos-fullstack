4fdda545dabe9d573e35b777780fe9d108685b42
Jaime Mejia-Beher <jmejiabeher@gmail.com>
2024-05-23 11:19:22 -0600

    added connection string to mongoDb

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,16 @@
 import express from "express";
+import mongoConfig from "./config.js";
+import 'dotenv/config'
 
 const app = express();
 
 const PORT = 8080
 
 app.get('/', (req, res) => {
-    res.json("Hello from server")
+    res.json("Hello from server...STILL WORKING")
 })
 
 app.listen(PORT, () => {
     console.log("Listening on PORT:", PORT)
+    mongoConfig() // will establish connection to database
 })
