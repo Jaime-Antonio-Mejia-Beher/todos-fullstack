e3b3615c5ee67c18544f0fe23330a036633147c7
Jaime Mejia-Beher <jmejiabeher@gmail.com>
2024-05-24 10:11:03 -0600

    added live link Base Url

--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -2,7 +2,10 @@ import { useState, useRef, useEffect } from "react";
 import "./App.css";
 // import { set } from "mongoose";
 
-const BASE_URL = "http://localhost:8080/api/todos";
+const BASE_URL = import.meta.env.DEV
+  ? "http://localhost:8080/api/todos"
+  : "https://todos-fullstack.onrender.com/api/todos";
+console.log(import.meta.env.DEV);
 
 function App() {
   const [isLoading, setIsLoading] = useState(false);
@@ -47,6 +50,8 @@ function App() {
       });
       const newTodo = await response.json();
       setTodos([...todos, newTodo]);
+      textRef.current.value = "";
+      completeRef.current.checkbox = false;
     } catch (error) {
       console.log(error);
     } finally {
@@ -58,16 +63,15 @@ function App() {
 
   async function handleDelete(id) {
     try {
-      setIsLoading(true)
-      await fetch(`${BASE_URL}/${id}`,{ method: 'DELETE'})
-      setTodos(todos.filter(todo => todo._id != id))
-    } catch(error) {
-      console.log(error)
+      setIsLoading(true);
+      await fetch(`${BASE_URL}/${id}`, { method: "DELETE" });
+      setTodos(todos.filter((todo) => todo._id != id));
+    } catch (error) {
+      console.log(error);
     } finally {
       setIsLoading(false);
     }
   }
-
 
   return (
     <>
@@ -97,7 +101,11 @@ function App() {
               {todo.text}
               <span
                 onClick={() => handleDelete(todo._id)}
-                style={{ marginLeft: "10px", fontWeight: "500", cursor: "pointer" }}
+                style={{
+                  marginLeft: "10px",
+                  fontWeight: "500",
+                  cursor: "pointer",
+                }}
               >
                 X
               </span>
